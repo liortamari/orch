76d7d7ffae838414862d664ceb660171447bf9c8
liortamary <lior.tamary@houzz.com>
2021-03-27 22:15:16 +0300

    fixes

--- orch.sql
+++ orch.sql
@@ -1,5 +1,6 @@
 CREATE USER 'orchestrator'@'%' IDENTIFIED BY 'orc_topology_password';
 GRANT SUPER, PROCESS, REPLICATION SLAVE, REPLICATION CLIENT, RELOAD ON *.* TO 'orchestrator'@'%';
 GRANT SELECT ON meta.* TO 'orchestrator'@'%';
+GRANT SELECT ON mysql.slave_master_info TO 'orchestrator'@'%';
 GRANT SELECT ON performance_schema.replication_group_members TO 'orchestrator'@'%';
 FLUSH PRIVILEGES;
